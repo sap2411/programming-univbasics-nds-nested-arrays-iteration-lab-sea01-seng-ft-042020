d9d24ec886495825ae845ff5dace0da4922b24ed
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-19 06:45:00 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -36,7 +36,7 @@ else
   row_index +=1
   end
 end
-  
+  row_index = 0
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
