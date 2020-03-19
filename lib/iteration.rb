2ce9270f977f679a17d8161801c3b7caa7fc8ed7
Steven Parsons <steven.parsons.us@gmail.com>
2020-03-19 06:46:13 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -37,6 +37,13 @@ else
   end
 end
   row_index = 0
+  while row_index < evenNum.count do
+  element_index = 0
+  while element_index < guessing_game_grid[row_index].count do
+    total += guessing_game_grid[row_index]
+    element_index += 1
+  end
+  row_index += 1
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
